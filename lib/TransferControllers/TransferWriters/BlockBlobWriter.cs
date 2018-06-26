c40c973f9c567977f05c9426d09b93f2858cbac1
Demid Sukhovsky <sukhovsky@hotmail.com>
2018-06-26 15:57:20 +1000

    fix zero-length blob bug

--- lib/TransferControllers/TransferWriters/BlockBlobWriter.cs
+++ lib/TransferControllers/TransferWriters/BlockBlobWriter.cs
@@ -354,10 +354,11 @@ namespace Microsoft.WindowsAzure.Storage.DataMovement.TransferControllers
                             transferData.Stream = new ChunkedMemoryStream(transferData.MemoryBuffer, 0, transferData.Length);
                         }
 
+                        var blockId = this.GetBlockId(transferData.StartOffset);
                         if (ShouldWriteBlock(transferData))
                         {
                             await Utils.ExecuteXsclApiCallAsync(async () => await this.blockBlob.PutBlockAsync(
-                                this.GetBlockId(transferData.StartOffset),
+                                blockId,
                                 transferData.Stream,
                                 null,
                                 Utils.GenerateConditionWithCustomerCondition(this.destLocation.AccessCondition, true),
